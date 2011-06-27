1f2a16cf91ee2b132dfb0f3ad03f5cec32360d7c
Greg DeKoenigsberg <gregdek@Greg-DeKoenigsbergs-MacBook.local>
2011-06-26 23:51:15 -0400

    Basic puzzle block level works!

--- resources/tutorial3/bundle-map-begin.js
+++ resources/tutorial3/bundle-map-begin.js
@@ -45,7 +45,10 @@
                                         [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
                                 ],
 
-                                // Define all objects added to the map.
+                                // Define all objects added to the map.  This is called once,
+				// when the level is first created, so all "init" code goes
+				// here.
+
                                 addObjects:function() {
 
 					// First, a different way of mapping the level.
@@ -55,19 +58,20 @@
 					// must be identical in size to "newmap".
 					var newmap=['xxxxxxxxxxxxxxx',
 					            'x.............x',
-					            'x...xxxxxx....x',
-					            'x...xx13xx....x',
-					            'x...x+22xx....x',
-					            'x...xx==xx....x',
-					            'x...xx.5xx....x',
+					            'x.............x',
+					            'x.....13......x',
+					            'x....+22......x',
+					            'x.....==......x',
+					            'x.....?5......x',
 					            'x.............x',
 					            'x.............x',
 					            'xxxxxxxxxxxxxxx'];
 					var newtiles={
-						"x":200, ".":201,
-						"1":101, "2":102, "3":103, "4":104, "5":105, 
-						"6":106, "7":107, "8":108, "9":109, "0":100,
-						"+":204, "-":205, "*":206, "=":207
+						"x":30, ".":31,
+						"1":11, "2":12, "3":13, "4":14, "5":15, 
+						"6":16, "7":17, "8":18, "9":19, "0":10,
+						"+":34, "-":35, "*":36, "=":37, 
+						"?":23  // there are 10 ?, 23 is the ? for "3"
 					}; 
 					for (ymap=0; ymap<newmap.length; ymap++) {
 						for (xmap=0; xmap<newmap[ymap].length; xmap++) {
@@ -105,6 +109,13 @@
 					
 					// maingame.setTileInMap(1,1,99,false);
 				},
+
+				// Now we define our "mapActions".  This function is called continually
+				// as part of the game loop, and conditions here are constantly being
+				// checked.  This is where all "game triggers" happen.  Is a player on
+				// a certain tile?  Did the player bump into a particular NPC?  And so
+				// on.
+
 				mapActions:function() {
 
 					// Get the player object to work with
@@ -117,6 +128,61 @@
 					// Get the ID of the tile the player is on
 					var ontile=help.getTileInMap(pl.x+pl.colx+pl.colhw,pl.y+pl.coly+pl.colhh,tilemaps.map,tilemaps._defaultblock,"map");
 
+
+					// Now we need to figure out: "is the block the player last touched 
+					// on a question mark?  And if so, is it the right one or the wrong one?"
+
+					// There are 10 question mark tiles that look identical.  The player 
+					// cannot tell the difference... but we can!
+
+					// In game-tutorial3.html, we set the puzzleblock object to set a
+					// global game variable, tilemaps.queststatus["last-pb-touched"],
+					// to be set every time a puzzleblock is touched.  This way, we
+					// know which puzzleblock to check the status of, and we don't have
+					// to go through every possible puzzleblock id.
+
+					// NOTE: the value is just an ID and does not match the value of
+					// the puzzleblock itself!  We need to get the "frame" value of
+					// the object, which holds the "digit" that we care about.
+
+					// We don't want to check if the player hasn't touched an object yet.
+					if (tilemaps.queststatus["last-pb-touched"]) {
+						var pb = gbox.getObject("walls",tilemaps.queststatus["last-pb-touched"]);
+                                	        var pbx=help.xPixelToTileX(tilemaps.map,pb.x+pb.colx+pb.colhw);
+                                        	var pby=help.yPixelToTileY(tilemaps.map,pb.y+pb.coly+pb.colhh);
+						var pbontile=help.getTileInMap(pb.x+pb.colx+pb.colhw,pb.y+pb.coly+pb.colhh,tilemaps.map,tilemaps._defaultblock,"map");
+						if (pbontile==23) {
+							// OK, the player has moved a puzzle block into place!
+							// Now we test to see if it was the right one!  Which,
+							// in this case, is "3".
+							if (pb.frame == 3) {
+								// Yes!  Destroy the object...
+								gbox.trashObject(pb);
+								// ...and set "last object touched" to false
+								tilemaps.queststatus["last-pb-touched"]=false;
+								// ...and add tile 13 here, which is the
+								// wall tile for the number 3.
+								maingame.setTileInMap(pbx,pby,13,false);
+							}
+							else {
+								// No.  Bad things happen!
+								maingame.addSmoke(pb);
+								gbox.trashObject(pb);
+								tilemaps.queststatus["last-pb-touched"]=false;
+								maingame.hud.addValue("health","value",-1); // Damage!
+                                                        	if (maingame.hud.getValue("health","value")<=0) // If dead..
+                                                                	pl.kill(); // Kill...
+                                                        	else { // Else is just hit
+                                                                	gbox.hitAudio("hurt");
+                                                                	pl.accz=-5; // A little jump...
+                                                                	pl.invultimer=30; // Stay invulnerable for a while...
+                                                                	pl.stilltimer=10; // Stay still for a while...
+								}
+
+							}
+						}
+					} 
+
 					// If he's in a cave, figure out which cave he's in and send him to the right dungeon.
 					if ((ontile==1) && (xc>=1) && (xc<=2)) maingame.gotoLevel({level:"gregdek-1-000",x:60,y:60,introdialogue:true,label:"Greg's First Quest"});
 					if ((ontile==1) && (xc>=4) && (xc<=5)) maingame.gotoLevel({level:"gregdek-2-000",x:60,y:60,introdialogue:true,label:"Greg's Second Quest"});
@@ -124,7 +190,8 @@
 					if ((ontile==1) && (xc>=11) && (xc<=14)) maingame.gotoLevel({level:"gregdek-4-000",x:60,y:60,introdialogue:true,label:"Greg's Fourth Quest"});
 					if ((ontile==1) && (xc>14)) maingame.gotoLevel({level:"generic-000",x:60,y:60,introdialogue:true,label:"Unclaimed Cave"});
 				 },
-				tileIsSolid:function(obj,t){ return ((t>=100) && (t<=200)) }
+				// Solid tiles: 10-19 (numbers) and 30 (wall).
+				tileIsSolid:function(obj,t){ return (((t>=10) && (t<=19)) || (t==30)) }
 			}
 		}
 	]
